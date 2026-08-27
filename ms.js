3822bf2bb795f24104535231c861f3f9628b73de
smatnpga <smatnpga@gmail.com>
2026-08-27 09:48:25 +0700

    first commit

---
+++ ms.js
@@ -0,0 +1,95 @@
+const b = {
+  dlsd1: "sd",
+  dlsmp2: "smp",
+  dlsma3: "sma",
+  dltdu4: "tdu",
+};
+
+const bo = {
+  dlsd: "sd",
+  dlsmp: "smp",
+  dlsma: "sma",
+  dltdu: "tdu",
+};
+
+const bode = {
+  dlsd01: "sd",
+  dlsmp92: "smp",
+  dlsma83: "sma",
+  dltdu74: "tdu",
+};
+
+const passwords = {
+  dlsd00: "sd",
+  dlsmp99: "smp",
+  dlsma88: "sma",
+  dltdu77: "tdu",
+};
+
+const permissions = {
+  dlsd101: "sd",
+  dlsmp292: "smp",
+  dlsma393: "sma",
+  dltdu484: "tdu",
+};
+
+const codess = {
+  dlsd10: "sd",
+  dlsmp29: "smp",
+  dlsma38: "sma",
+  dltdu47: "tdu",
+};
+
+const cods = {
+  dlsd11: "sd",
+  dlsmp22: "smp",
+  dlsma33: "sma",
+  dltdu44: "tdu",
+};
+
+const c = {
+  klsd1: "sd",
+  klsmp2: "smp",
+  klsma3: "sma",
+  kltdu4: "tdu",
+};
+
+const co = {
+  klsd: "sd",
+  klsmp: "smp",
+  klsma: "sma",
+  kltdu: "tdu",
+};
+
+const code = {
+  klsd01: "sd",
+  klsmp92: "smp",
+  klsma83: "sma",
+  kltdu74: "tdu",
+};
+
+const password = {
+  klsd00: "sd",
+  klsmp99: "smp",
+  klsma88: "sma",
+  kltdu77: "tdu",
+};
+
+const permission = {
+  klsd101: "sd",
+  klsmp292: "smp",
+  klsma393: "sma",
+  kltdu484: "tdu",
+};
+
+const codes = {
+  "2627_s1_tnpga10": "2627_s1_sma",
+  "2526_s1_tnpga29": "coming",
+};
+
+const cod = {
+  klsd11: "sd",
+  klsmp22: "smp",
+  klsma33: "sma",
+  kltdu44: "tdu",
+};
